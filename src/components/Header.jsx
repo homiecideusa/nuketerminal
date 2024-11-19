74cf92e48377eac851e000852e3f695b9062d752
solanaenjoyer <patrykkoak@gmail.com>
2024-11-19 21:05:17 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,7 @@ import '../css/Header.styles.css'
 const Header = () => {
     return (
         <nav>
-            <div className="links-container">
-                <ul id="menu">
-                    <li data-menuanchor="logon-hint" id="logo-hint"><a href="https://pump.fun/coin/" target="_blank" rel="noopener noreferrer">Buy Now</a></li>
-                    <li data-menuanchor="twitter" className='social'>
-                        <a href="https://x.com/16zcore" className="social-image-container">
-                            <img src="/assets/images/socials/twitter-icon-white.png" alt="Twitter image" />
-                        </a>
-                    </li>
-                </ul>
-            </div>
+          
         </nav>
       )
 }
