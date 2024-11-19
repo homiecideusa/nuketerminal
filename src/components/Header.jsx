3b777aac9f7cbfe72a1bce0599e6b5c6383353ed
solanaenjoyer <patrykkoak@gmail.com>
2024-11-19 20:37:45 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
         <nav>
             <div className="links-container">
                 <ul id="menu">
-                    <li data-menuanchor="logon-hint" id="logo-hint"><a href="https://pump.fun/coin/7MTsnvGFEVUCqY1K2aQhCeXCQ7ijymQG24WbrLopump" target="_blank" rel="noopener noreferrer">Buy Now</a></li>
+                    <li data-menuanchor="logon-hint" id="logo-hint"><a href="https://pump.fun/coin/" target="_blank" rel="noopener noreferrer">Buy Now</a></li>
                     <li data-menuanchor="twitter" className='social'>
-                        <a href="https://x.com/nuketerminal" className="social-image-container">
+                        <a href="https://x.com/16zcore" className="social-image-container">
                             <img src="/assets/images/socials/twitter-icon-white.png" alt="Twitter image" />
                         </a>
                     </li>
