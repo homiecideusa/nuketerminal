012fb39aa03e9dff164b05ea86e11762ec1329db
solanaenjoyer <patrykkoak@gmail.com>
2024-11-19 20:36:43 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,6 @@ import '../css/Footer.styles.css'
 const Footer = () => {
     return (
         <footer>
-            <a href="">2054</a>
-            <a href="">POWERED BY CABAL</a>
         </footer>
       )
 }
